084df61fa50ea95e8d497da7e862f0e3ed4ded78
Gauti20 <gautier.tiehoule@ngser.com>
2025-02-05 10:33:08 +0000

    Fix: Data Validator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, BackgroundTasks, Request
+from fastapi import FastAPI, BackgroundTasks, Request, Form
 from fastapi.middleware.cors import CORSMiddleware 
-from typing import Union
+from typing import Optional, Annotated
 from pydantic import BaseModel
 from httpx import AsyncClient, RequestError, TimeoutException
 from starlette import status
@@ -11,22 +11,22 @@ from datetime import datetime
 
 class NotificationRequest(BaseModel):
     url: str
-    order_id: str
-    status_id: int
-    transaction_id: str
-    transaction_amount: str
-    currency: str
-    paid_transaction_amount: str
-    paid_currency: str
-    change_rate: float
-    conflictual_transaction_amount: str
-    conflictual_currency: str
-    wallet: str
-    phone_number: Union[str, None] = None
-    momo_reference_id: Union[str, None] = None
-    wallet_alias: str
-    wallet_name: str
-    receipt_generator_url: str
+    order_id: Optional[str] = None
+    status_id: Optional[int] = None
+    transaction_id: Optional[str] = None
+    transaction_amount: Optional[str] = None
+    currency: Optional[str] = None
+    paid_transaction_amount: Optional[str] = None
+    paid_currency: Optional[str] = None
+    change_rate: Optional[float] = None
+    conflictual_transaction_amount: Optional[str] = None
+    conflictual_currency: Optional[str] = None
+    wallet: Optional[str] = None
+    phone_number: Optional[str] = None
+    momo_reference_id: Optional[str] = None
+    wallet_alias: Optional[str] = None
+    wallet_name: Optional[str] = None
+    receipt_generator_url: Optional[str] = None
     cancellation: bool
 
 app = FastAPI()
@@ -45,12 +45,12 @@ app.add_middleware(
 async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
     if exc.status_code == 404:
         return JSONResponse(
-            status_code=404,
-            content={"detail": "Resource not found"},
+            status_code = 404,
+            content = {"detail": "Resource not found"},
         )
     return JSONResponse(
-        status_code=exc.status_code,
-        content={"detail": exc.detail},
+        status_code = exc.status_code,
+        content = {"detail": exc.detail},
     )
 
 
@@ -64,20 +64,20 @@ async def send_notifiation(request: NotificationRequest):
     url = request.url
     data = request.model_dump()
     data.pop('url')
-    print(f"[{datetime.now()}]URL => {url} \n[{datetime.now()}]PARAMS SENT => {data} \n")
+    print(f"[{datetime.now()}] URL => {url} \n[{datetime.now()}] PARAMS SENT => {data} \n")
     
     try:
         async with AsyncClient(timeout=30.0) as client: 
             response = await client.post(url, headers = headers, data = data )
-            print(f"[{datetime.now()}]RESPONSE => #{response.json()}")
+            print(f"[{datetime.now()}] RESPONSE => #{response.json()}")
     except TimeoutException:
-        print(f"[{datetime.now()}]RESPONSE TO {url} TIMED OUT.")
+        print(f"[{datetime.now()}] RESPONSE TO {url} TIMED OUT.")
     except RequestError as e:
-        print(f"[{datetime.now()}]An error occurred while making the request: {e}")
+        print(f"[{datetime.now()}] An error occurred while making the request: {e}")
     
     
 @app.post("/", status_code = status.HTTP_202_ACCEPTED )
-async def root(request: NotificationRequest, background_tasks: BackgroundTasks):
+async def root(request: Annotated[NotificationRequest, Form()], background_tasks: BackgroundTasks):
     background_tasks.add_task(send_notifiation, request)
     return {"status": 200,"message": "Notification Sent"}
     
